d44f581f2ebc4e7b574c4c7c4a9e9d2ecd3d9c55
popjy0312 <popjy0312@naver.com>
2019-02-27 13:37:58 +0900

    주석추가 링크 변경

--- eguru_downloader.py
+++ eguru_downloader.py
@@ -16,7 +16,7 @@ from bs4 import BeautifulSoup
 import urllib
 import re
 
-SAVE_DIR = "D:\\movie\\진격의거인\\".decode('utf-8')
+SAVE_DIR = "D:\\영화,만화,드라마,애니\\진격의거인_만화\\".decode('utf-8')
 
 ########################################################
 #
@@ -28,7 +28,7 @@ SAVE_DIR = "D:\\movie\\진격의거인\\".decode('utf-8')
 
 base_url = "https://eguru.tumblr.com"
 
-main = urlopen("https://eguru.tumblr.com/%EC%A7%84%EA%B2%A9%EC%9D%98-%EA%B1%B0%EC%9D%B8").read()		#진격의거인
+main = urlopen("https://eguru.tumblr.com/shingeki-no-kyojin").read()		#진격의거인
 
 bs = BeautifulSoup(main, 'html.parser')
 
@@ -46,6 +46,7 @@ book_links =  re.findall('href=\"(.*?)\" target', str(body))
 #
 ########################################################
 
+
 if not os.path.exists(SAVE_DIR):
     os.mkdir(SAVE_DIR)
 os.chdir(SAVE_DIR)
@@ -53,9 +54,9 @@ os.chdir(SAVE_DIR)
 for now_link in book_links:
 	book_name = unquote(now_link.split('/')[-1])
 	print book_name + " download start"
-	if os.path.exists(book_name.decode('utf-8')):		#if already downloaded
-		continue
-	os.mkdir(book_name.decode('utf-8'))
+	if os.path.exists(book_name.decode('utf-8')):		# if already downloaded
+		continue										# 폴더 존재하면 이미 다운로드한것으로 간주하고 스킵시켰음.
+	os.mkdir(book_name.decode('utf-8'))					# 책이름으로 폴더 생성
 	os.chdir(book_name.decode('utf-8'))
 	_url = base_url + now_link
 	# print _url
@@ -73,6 +74,8 @@ for now_link in book_links:
 
 	for i in range(0,len(image_links)):
 		print book_name + '_' + str(i) + " downloading"
-		urllib.urlretrieve(image_links[i], book_name.decode('utf-8') + '_' + str(i) + '.' + image_links[i].split('.')[-1])	# 다운링크 마지막 파일포맷(.jpg, .png 등 포함 저장)
+		urllib.urlretrieve(image_links[i], book_name.decode('utf-8') + '_' + str(i) + '.' + image_links[i].split('.')[-1])	
+			# 저장 파일명 예시: 진격의-거인-1권_0.jpg, 진격의-거인-1권_1.jpg, ...
+			# 다운링크 마지막 파일포맷(.jpg, .png 등 포함 저장)
 
 	os.chdir("..")
